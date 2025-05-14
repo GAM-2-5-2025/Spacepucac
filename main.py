9377d34734fdb0a682d92a4f662af3d02204b423
Marta <95877609+AtramLj@users.noreply.github.com>
2025-05-14 13:51:19 +0200

    Update main.py
    8:00

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
-import pygame
-
-pygame.init()
-
-window_width = 1000
-window_height = 700
+import pygame,sys
+from player import Player
 
 
-pygame.display.set_mode((window_width, window_height))
-screen = pygame.display.set_mode((window_width, window_height))
-
-pozadina = pygame.image.load('pozadina.png').convert()
-
-pozadina = pygame.transform.scale(pozadina, screen.get_size())
-
-#glavno
-run = True
-x=0
-y=0
-vrhy=-window_height
-while run:
-    screen.fill((0,0,0))
+class Game:
+    def __init__(self):
+        player_sprite = Player((300,300))
+        self.player = pygame.sprite.GroupSingle(player_sprite)
+    def run(self):
+        self.player.draw(screen)
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-    y=y+0.1
-    vrhy = vrhy+0.1
-    screen.blit(pozadina,(x,y))
-    screen.blit(pozadina,(x,vrhy))
-    if vrhy>=0:
-        y=0
-        vrhy=-window_height
+if __name__ == '__main__':
+    pygame.init()
+
+    window_width = 1000
+    window_height = 700
+
+
+    pygame.display.set_mode((window_width, window_height))
+    screen = pygame.display.set_mode((window_width, window_height))
+
+    pozadina = pygame.image.load('pozadina.png').convert()
+
+    pozadina = pygame.transform.scale(pozadina, screen.get_size())
+
+    #glavno
+    run = True
+    x=0
+    y=0
+    vrhy=-window_height
+    game = Game()
+    
+    while run:
         
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    pygame.display.update()
+        screen.fill((0,0,0))
+        game.run()
+        y=y+0.1
+        vrhy = vrhy+0.1
+        screen.blit(pozadina,(x,y))
+        screen.blit(pozadina,(x,vrhy))
+        if vrhy>=0:
+            y=0
+            vrhy=-window_height
+            
+
+        pygame.display.update()
 
 
-pygame.quit()
+    pygame.quit()

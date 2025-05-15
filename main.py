899dd1448662163284a7f7fa8100a4e5201bc6a9
Marta <95877609+AtramLj@users.noreply.github.com>
2025-05-15 22:50:59 +0200

    Update main.py
    player i objekti

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
-import pygame,sys
+import pygame
+import random
+import sys
 from player import Player
 
 
 class Game:
     def __init__(self):
-        player_sprite = Player((300,300))
+        player_sprite = Player((window_width/2,window_height),window_width,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
+
+    #VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+        self.objects = pygame.sprite.Group()
+        self.spawn_timer = pygame.time.get_ticks()
+        
+    def stvori_objekt(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.spawn_timer > 1000:  
+            self.objects.add(Objekti(window_width))
+            self.spawn_timer = current_time
+    ##AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            
     def run(self):
+        self.player.update()
         self.player.draw(screen)
-    
+
+        #VVVVVVVVVVVVVVV
+        self.stvori_objekt()
+        self.objects.update()
+        self.objects.draw(screen)
+        #AAAAAAAAAAAAAAAAAAAAAA
+
+#VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+class Objekti(pygame.sprite.Sprite):
+    def __init__(self, window_width):
+        super().__init__()
+        self.image = pygame.Surface((30, 30))
+        self.image.fill((255, 0, 0))  
+        self.rect = self.image.get_rect(
+            midtop=(random.randint(0, window_width), -30)
+        )
+        self.speed = 1
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top > window_height:  
+            self.kill()  
+##AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 if __name__ == '__main__':
     pygame.init()
 
-    window_width = 1000
+    window_width = 700
     window_height = 700
+
+    clock = pygame.time.Clock()
 
 
     pygame.display.set_mode((window_width, window_height))
@@ -38,17 +77,19 @@ if __name__ == '__main__':
                 run = False
 
         screen.fill((0,0,0))
-        game.run()
         y=y+0.1
         vrhy = vrhy+0.1
         screen.blit(pozadina,(x,y))
         screen.blit(pozadina,(x,vrhy))
+        game.run()
+        
         if vrhy>=0:
             y=0
             vrhy=-window_height
             
 
         pygame.display.update()
+        clock.tick(60)
 
 
     pygame.quit()

df67191ecf5d30479efa1af7625cd155f16b9a94
Marta <95877609+AtramLj@users.noreply.github.com>
2025-05-15 22:51:24 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,51 @@
 import pygame
+import sys
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos,constraint,speed):
         super().__init__()
-        self.image = pygame.image.load('igrac.png').covert_alpha()
+        self.image = pygame.image.load('igrac.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = speed
+        self.max_x= constraint
+        self.ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 600
+        
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.rect.x -=self.speed
+        if keys[pygame.K_SPACE] and self.ready:
+            self.shoot()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks()
+
+    def recharge(self):
+        if not self.ready:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_time >= self.laser_cooldown:
+                self.ready = True
+                
+                
+
+    def constraint(self):
+        if self.rect.left <=0:
+            self.rect.left = 0
+        if self.rect.right >=self.max_x:
+            self.rect.right = self.max_x
+
+    def shoot(self):
+        print('sutaj!!!')
+    
+    def update(self):
+        self.get_input()
+        self.constraint()
+        self.recharge()
+        
+
+        
